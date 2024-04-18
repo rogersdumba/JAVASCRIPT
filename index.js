cd411af4610595e03e321a2a47de51823dd84107
rogersdumba <132753660+rogersdumba@users.noreply.github.com>
2024-04-18 22:55:21 +0300

    Text-Animation done

---
+++ index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container")
+
+
+const careers = ["Android Developer", "iOS Developer", "Designer"]
+let careerIndex = 0;
+let characterIndex = 0;
+
+updateText()
+function updateText() {
+    characterIndex++;
+    container.innerHTML = `<h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+    if (characterIndex === careers[careerIndex].length) {
+        careerIndex++;
+        characterIndex = 0;
+    }
+
+    if (careerIndex === careers.length) {
+        careerIndex = 0;
+        characterIndex = 0;
+    }
+    setTimeout(updateText, 400);
+}
+
